9ed5a1334b730e47b3ae96a5dba3c201385f78f6
Vicenzo7 <deeprajm35@gmail.com>
2023-01-07 21:19:34 +0530

    RabbitMQ config

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,49 +1,62 @@
 const { StatusCodes } = require("http-status-codes");
 const { BookingService } = require("../services/index");
 
+const { createChannel, publishMessage } = require("../utils/messageQueue");
+const { REMINDER_BINDING_KEY } = require("../config/serverConfig");
+
 const bookingService = new BookingService();
 
-const create = async (req, res) => {
-  try {
-    const response = await bookingService.createBooking(req.body);
+class BookingController {
+  constructor() {}
+
+  async sendMessageToQueue(req, res) {
+    const channel = await createChannel();
+    const data = { message: "Success" };
+    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
     return res.status(StatusCodes.OK).json({
-      data: response,
-      success: true,
-      message: "Successfully completed booking",
-      err: {},
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(error.statusCodes).json({
-      data: {},
-      success: false,
-      message: error.message,
-      err: error.explanation,
+      message: "Successfully published the event",
     });
   }
-};
 
-const update = async (req, res) => {
-  try {
-    const response = await bookingService.updateBooking(req.params.id);
-    return res.status(StatusCodes.OK).json({
-      data: response,
-      success: true,
-      message: "Successfully cancelled the booking",
-      err: {},
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(error.statusCodes).json({
-      data: {},
-      success: false,
-      message: error.message,
-      err: error.explanation,
-    });
+  async create(req, res) {
+    try {
+      const response = await bookingService.createBooking(req.body);
+      return res.status(StatusCodes.OK).json({
+        data: response,
+        success: true,
+        message: "Successfully completed booking",
+        err: {},
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(error.statusCodes).json({
+        data: {},
+        success: false,
+        message: error.message,
+        err: error.explanation,
+      });
+    }
   }
-};
 
-module.exports = {
-  create,
-  update,
-};
+  async update(req, res) {
+    try {
+      const response = await bookingService.updateBooking(req.params.id);
+      return res.status(StatusCodes.OK).json({
+        data: response,
+        success: true,
+        message: "Successfully cancelled the booking",
+        err: {},
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(error.statusCodes).json({
+        data: {},
+        success: false,
+        message: error.message,
+        err: error.explanation,
+      });
+    }
+  }
+}
+
+module.exports = BookingController;
